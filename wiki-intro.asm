c132fd3df80b08d06c07bdba1cf227948c731c09
themaxwiki <kill99999@outlook.it>
2025-03-24 21:35:01 +0100

    Update wiki-intro.asm

--- wiki-intro.asm
+++ wiki-intro.asm
@@ -5,3 +5,11 @@ Se il vostro obbiettivo è creare un sito semplice e funzionale, siete nel posto
 The Max Wiki semplifica il processo di sviluppo delle wiki.
 
 Per iniziare, premete il pulsante crea!
+
+Attenzione: Le wiki, prima di essere pubblicate vengono controllate dal nostro team.
+
+Se create diversi file con una quantità significativa di testo, il nostro team ci metterà più tempo per analizzare la vostra wiki.
+
+Se inviate delle e-mail al nostro team per richiedere un sollecito del processo di verifica delle wiki la vostra richiesta verrà ignorata e la wiki verrà cancellata.
+
+(C) Copyright 2025 The Max Wiki. Tutti i diritti sono riservati.
